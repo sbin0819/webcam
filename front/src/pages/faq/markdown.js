3c7669372470519a10fc0a5ff51bb425fd173f1f
sbinha0819 <sbinha123@gmail.com>
2020-09-13 18:38:16 +0900

    init policies

---
+++ front/src/pages/faq/markdown.js
@@ -0,0 +1,121 @@
+export const markdown = `
+<h1 class='top'>FAQ/h1>
+<h1>Overview</h1>
+<p>The real magic of Focusmate is our community! Each of us encourages, inspires, and depends on one another to accomplish amazing things.</p>
+
+<p>We ask each member to join us in building the most supportive community on earth. This guide will help you do that, covering:</p>
+
+<li>Community Pledge</li>
+<li>How To Be Productive On Focusmate</li>
+<li>Forbidden Behaviors</li>
+<li>What To Do If You Encounter Bad Behavior</li>
+<li>Have questions? Don’t worry! Send us your feedback.</li>
+<br/>
+
+<h1>Community Pledge</h1>
+<p>Creating a safe and supportive Focusmate environment comes down to how each one of us shows up for each other, day to day, moment to moment.</p>
+
+<p>We invite you to make the following pledge:</p>
+
+1.Honor your commitments. Your partner depends on you to show up on time, to have your video on, and to stay for your whole session.
+
+2.Be kind. Your partner is here to work hard and aim high, so cheer them on! Be encouraging and positive!
+
+3.Be professional. Treat your partner as if they were your most important client, mentor, or colleague. Remember we’re a professional community and not a dating site.
+
+4.Protect your attention. Help your partner stay focused by removing distractions. Don’t check your phone—try putting it in another room! Keep your conversation to the beginning and end of your session. And don’t socialize with other people around you.
+
+5.Work with rigor and intention. Inspire your partner’s work with your own. Clearly communicate concrete goals and work hard on them. Celebrate what you and your partner have accomplished together!
+<br/>
+
+<h1>How to Be Productive on Focusmate</h1>
+
+<p>You’re here to get more done that ever before. And we’re here to help! Most members say they are over 50% more productive on Focusmate.</p>
+
+
+<p>Here are our recommendations on how to be as productive as possible.</p>
+
+<h2>Prepare for your session</h2>
+
+1.Turn off your phone and put it out of sight
+
+2.Remove any other sources of interruption: close your door, turn off notifications, etc.
+
+3.Get water/coffee or use the restroom
+
+4.Open your session 2-3 minutes before it starts
+
+5.Make sure you are visible in the video
+
+
+<h2>Kick off your session</h2>
+
+1.Be friendly and greet your partner
+
+2.Ask your partner: “What are you planning to accomplish this session?”
+
+3.Spend about 30 seconds describing the specific tasks you'll work on during your session
+
+4.Break your commitment down into specific tasks
+
+5.Post your plan in the chat area
+
+
+<h2>Start working</h2>
+
+1.Get to work
+
+2.Work quietly on your tasks
+
+3.Post updates in chat as you move through your list
+
+4.If you need to take a bio break, post in chat
+
+<h2>Wrap up</h2>
+
+1.After the session ends, check in with your partner to hear about their session
+
+2.Be supportive and celebrate your productive session! Enthusiasm is contagious!
+
+`;
+
+const extra = `Forbidden Behaviors
+Focusmate is an amazing community, but the internet is a big place, and sometimes people use it irresponsibly. We’ve put these rules in place to keep you safe. If you don’t feel safe or if someone breaks one of these rules, please let us know. We’ve got your back—and so do 99.99% of the people using Focusmate!
+
+These rules apply to all interactions on Focusmate—in sessions, in all community forums, and at all Focusmate events.
+
+BREAKING THESE RULES WILL RESULT IN A BAN:
+No nudity, sexual acts, or sexual harassment. No nudity, no sexual acts, and nothing sexually suggestive at all.
+No threats, violence, or harm. Don’t threaten anyone in any way. Don’t harm someone else or yourself.
+No hateful conduct or harassment. Don’t harass, bully, or shame anyone. Don't do anything that demeans, defames, or promotes discrimination or violence on the basis of gender identity and expression, race, ethnicity, national origin, age, sexual orientation, disability, physical appearance, body size, religion, or veteran status.
+No flirting or inappropriate comments. Don’t be flirty or make suggestive remarks. Keep the conversation focused on work. Don’t ask for your partner’s full name, exact location, age, or anything else that’s personally identifiable.
+No inappropriate attire. You don’t need to dress up, but you should be appropriately covered up, as you would be to go to a cafe, including wearing a shirt and shorts/pants/skirt at all times.
+No selling. Don’t use Focusmate to sell or promote a product, service, or brand. No “soft-selling,” i.e., promoting content or a brand you are affiliated with.
+What To Do If You Encounter Bad Behavior
+What should you do if you encounter bad behavior on Focusmate? Follow these 3 steps.
+
+1. Take care of yourself
+If you ever feel unsafe or uncomfortable, please take care of yourself first. Trust yourself and your intuition.
+
+As a reminder, you can always close your browser tab to quickly remove the source of your distress.
+
+2. Report the offending member
+Please report any bad behavior you experience.
+
+Reporting bad behavior allows us to take action against the offender. It creates a safer community for you and prevents others from having the same bad experience you did.
+
+What happens if you report someone?
+
+Your report will only be shared with Focusmate and it is completely confidential.
+We’ll review your report and get back to you within 24 hours, with information about how we handle this type of incident. Expect a much faster response for high-severity incidents.
+Depending on the severity of the rule your partner broke, they’ll either get warned, suspended, or banned.
+3. Block the offending member
+You can also block that person immediately.
+
+Blocking prevents someone from:
+
+Booking sessions with you
+Seeing your sessions on the calendar
+Messaging you
+Seeing your name or profile anywhere on Focusmate
+You can block them from within a session, from their profile, or from your session details pop-up.`;
